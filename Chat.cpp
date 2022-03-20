a9e819436e6b684e6650c443269d8d8793ac5c37
SnR6 <83140169+SnR6@users.noreply.github.com>
2022-03-20 14:00:23 +0300

    OnlineChat
    C++; SFML; UDP Sockets;

---
+++ Chat.cpp
@@ -0,0 +1,31 @@
+﻿//#include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
+#include <Windows.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+using namespace sf;
+
+#include "User.h"
+#include "Client.h"
+#include "Server.h"
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	char ct = 's';
+	cout << "s / c \n"; // s - server / c - client
+	cin >> ct;
+
+	if (ct == 's') {
+		Server::Startup();
+	}
+	else if (ct == 'c') {
+		Client::Startup();
+	}
+}
